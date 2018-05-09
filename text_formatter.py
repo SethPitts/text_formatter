92c2f5257796977556ce1f6ddfd8694e9abf8ce2
sethpitts22@gmail.com <sethpitts22@gmail.com>
2018-05-09 19:59:28 -0400

    First Commit

---
+++ text_formatter.py
@@ -0,0 +1,58 @@
+def format_by_charater_length(max_length: int, text: str):
+    # split text by " " to find words
+    words_in_text = iter(text.split(" "))
+    current_length = 0
+    previous_word = next(words_in_text)
+    next_word = next(words_in_text)
+    updated_words = []
+    try:
+        while True:
+            current_length += len(previous_word) + 1  # add one for space
+            if current_length >= max_length:
+                previous_word += "\n"
+                current_length = 0
+            updated_words.append(previous_word)
+            previous_word = next_word
+            next_word = next(words_in_text)
+    except StopIteration:
+        updated_words.append(previous_word)
+
+    print(text, "old")
+    print(" ".join(updated_words), "updated")
+
+
+def format_by_charater_length_with_keyword(max_length: int, text: str, keyword: str):
+    # split text by " " to find words
+    total_lenth = max_length + len(keyword) + 1 # add one for space after keyword
+    words_in_text = iter(text.split(" "))
+    current_length = 0
+    previous_word = next(words_in_text)
+    next_word = next(words_in_text)
+    updated_words = []
+    try:
+        while True:
+            current_length += len(previous_word) + 1  # add one for space after word
+            if current_length >= total_lenth:
+                previous_word += "\n{}".format(keyword)
+                current_length = 0
+            updated_words.append(previous_word)
+            previous_word = next_word
+            next_word = next(words_in_text)
+    except StopIteration:
+        updated_words.append(previous_word)
+
+    # add keyword to first word
+    updated_words[0] = "COMMENT " +  updated_words[0]
+    print(text, "old")
+    print(" ".join(updated_words), "updated")
+
+
+def main():
+    text = "This req file was created by Seth Pitts on 09MAY2018 for ZC1/CHP"
+
+    format_by_charater_length(50, text)
+    format_by_charater_length_with_keyword(30, text, "COMMENT")
+
+
+if __name__ == '__main__':
+    main()
